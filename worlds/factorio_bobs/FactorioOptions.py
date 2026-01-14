64dbe73b380e04f075278c69f71cd9095fbe0778
CosmicWolf <54233835+Enderdraak@users.noreply.github.com>
2026-01-14 20:16:10 +0100

    Capitalization

--- worlds/factorio_bobs/FactorioOptions.py
+++ worlds/factorio_bobs/FactorioOptions.py
@@ -182,7 +182,7 @@ class TechTreeLayout(Choice):
     Funnels: Several bottom halves of diamonds (6/10/15 nodes each)
     Trees: Several trees
     Choices: A single balanced binary tree
-    irregular: starts wtih the 5 tech unlocked and randomly assigns dependencies to techs after.
+    Irregular: Starts wtih the 5 tech unlocked and randomly assigns dependencies to techs after.
     """
     display_name = "Technology Tree Layout"
     option_single = 0
