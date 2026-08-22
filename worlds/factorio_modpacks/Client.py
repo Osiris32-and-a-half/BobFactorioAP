9939fc8de27804da176afdfa3e69a20684baaaed
Osiris32-and-a-half <64905829+osiris32-and-a-half@users.noreply.github.com>
2026-08-22 17:46:07 +0100

    fixed getting settings for client

--- worlds/factorio_modpacks/Client.py
+++ worlds/factorio_modpacks/Client.py
@@ -641,7 +641,7 @@ class FactorioJSONtoTextParser(JSONtoTextParser):
 
 
 factorio_server_logger = logging.getLogger("FactorioServer")
-settings: FactorioSettings = get_settings().factorio_bobs_options
+settings: FactorioSettings = get_settings().factorio_modpacks_options
 if os.path.samefile(settings.executable, sys.executable):
     selected_executable = settings.executable
     settings.executable = FactorioSettings.executable  # reset to default
